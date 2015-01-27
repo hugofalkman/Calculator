5390628dd99af387a9cb947c96be5a359744a91a
H Hugo Falkman <zilla@Hs-MacBook-Pro.local>
2015-01-27 21:05:03 +0100

    All buttons in lecture 2 work

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTyping = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsTyping {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsTyping = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsTyping {
+            enter()
+        }
+        switch operation {
+        case "×": performOperation() {$0 * $1}
+        case "÷": performOperation() {$1 / $0}
+        case "+": performOperation() {$0 + $1}
+        case "−": performOperation() {$1 - $0}
+        case "√": performOperation() {sqrt($0)}
+        default: break
+        }
     }
+    
+    func performOperation(operation: (Double,Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = [Double]()
 
-
+    @IBAction func enter() {
+        userIsTyping = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsTyping = false
+        }
+    }
 }
+
+
+
+
+
+
+
+
 
