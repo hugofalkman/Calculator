4332467430df47027c1ae51e5ee71a7890c09296
H Hugo Falkman <zilla@Hs-MacBook-Pro.local>
2015-02-01 15:22:27 +0100

    Lesson 3 complete

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     var userIsTyping = false
     
+    var brain = CalculatorBrain()
+    
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsTyping {
@@ -29,36 +31,22 @@ class ViewController: UIViewController {
         if userIsTyping {
             enter()
         }
-        switch operation {
-        case "×": performOperation() {$0 * $1}
-        case "÷": performOperation() {$1 / $0}
-        case "+": performOperation() {$0 + $1}
-        case "−": performOperation() {$1 - $0}
-        case "√": performOperation() {sqrt($0)}
-        default: break
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation){
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
         }
     }
-    
-    func performOperation(operation: (Double,Double) -> Double) {
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    func performOperation(operation: Double -> Double) {
-        if operandStack.count >= 1 {
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    var operandStack = [Double]()
 
     @IBAction func enter() {
         userIsTyping = false
-        operandStack.append(displayValue)
-        println("operandStack = \(operandStack)")
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
     }
     
     var displayValue: Double {
