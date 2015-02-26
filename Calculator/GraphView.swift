d9afe5a6684775c591eac0bfc7a717bc9d99d7be
hugofalkman <zilla@Hs-MacBook-Pro.local>
2015-02-26 14:11:13 +0100

    Generic graphing view drawing the axes I(only)

--- Calculator/GraphView.swift
+++ Calculator/GraphView.swift
@@ -8,9 +8,38 @@
 
 import UIKit
 
+@IBDesignable
 class GraphView: UIView {
-
-    override func drawRect(rect: CGRect) {
+    
+    override var contentScaleFactor: CGFloat {didSet {setNeedsDisplay()}}
+    
+    var scaleSize: CGFloat = 0.9 {didSet {setNeedsDisplay()}}
+    var scaleOrigin: CGFloat {
+        return ((1 - scaleSize) / 2)
+    }
+    
+    let axesdrawer = AxesDrawer(color: UIColor.blackColor())
+    
+    var origo: CGPoint {
+        get {return convertPoint(center, fromView: superview)}
+        set {setNeedsDisplay()}
+    }
+    var scale: CGFloat = 25 {didSet {setNeedsDisplay()}}
+    
+    func viewDidLoad() {
+        axesdrawer.contentScaleFactor = self.contentScaleFactor
+        let test: CGRect = bounds
         
     }
+    
+    override func drawRect(rect: CGRect) {
+        var drawBounds = bounds
+        drawBounds.size.width *= scaleSize
+        drawBounds.size.height *= scaleSize
+        drawBounds.origin.x += drawBounds.size.width * scaleOrigin
+        drawBounds.origin.y += drawBounds.size.height * scaleOrigin
+        axesdrawer.drawAxesInRect(drawBounds, origin: origo, pointsPerUnit: scale)
+    }
 }
+
+        
